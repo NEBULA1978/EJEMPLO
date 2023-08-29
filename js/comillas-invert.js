4e7ed94c623bc101e597af0a08599cd677e2e73b
NEBULA1978 <rpascualey@gmail.com>
2023-08-29 23:39:58 +0200

    Practicando con botones y javascript eventos añadiendo variables

--- js/comillas-invert.js
+++ js/comillas-invert.js
@@ -1,6 +1,13 @@
 let nombre = 'Jab';
 const CANAL = "HTML6";
 let fecha = new Date();
+let numero = 3;
+let euros=100;
 
-let mensaje = `Me llamo ${nombre}, mi canal es ${CANAL} y estamos en el año ${fecha.getFullYear()}`;
+let mensaje = `Me llamo ${nombre}, mi canal es ${CANAL} y estamos en el año ${fecha.getFullYear()} y despues del numero ${numero} viene el numero ${++numero} y el valor de ${euros}€ equivalen a ${convertir(euros)}  pesos mexicanos`;
 alert(mensaje);
+
+function convertir(a){
+    let resultado= a * 22.23;
+    return resultado;
+}
